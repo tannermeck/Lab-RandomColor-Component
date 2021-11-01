4814f2a53aad0052c031cd20ae7fe703b91c4447
TANNER MECK <steve.meck246@gmail.com>
2021-11-01 13:36:04 -0700

    color changer component now rendering in app.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import ColorChanger from '../color-changer/ColorChanger';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <ColorChanger />;
 }
---
+++ src/components/color-changer/ColorChanger.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ColorChanger extends Component {
+
+    render() {
+        return (
+            <>
+                <h1>Hello From ColorChanger</h1>
+            </>
+        )
+    }
+}
+
+export default ColorChanger;
