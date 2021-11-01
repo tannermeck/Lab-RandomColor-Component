51bd972db826a00533473b5d83e5cbbc5a60c08f
TANNER MECK <steve.meck246@gmail.com>
2021-11-01 14:33:48 -0700

    color is randomly changed, but with each refresh

--- src/containers/RandomColor.jsx
+++ src/containers/RandomColor.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
 class RandomColor extends Component {
+    state = { colors: ['blue', 'green', 'red', 'orange', 'yellow', 'purple', 'grey'],
+                currentColor: 'blue' }
+
+    // handleColor = (colors) => { 
+    //     this.setState({ currentColor: colors})
+    // }
 
     render() {
+        const randomNumber = Math.floor(Math.random() * 6)
+        const randomColor = this.state.colors[randomNumber]
+        console.log(randomColor)
         return (
             <>
-                <h1>Hello From Random Color</h1>
+                <h1>Hello From Random Color Generator</h1>
+                <div style={{ backgroundColor: randomColor, height: 300, width: 300 }}>
+                </div>
             </>
         )
     }
