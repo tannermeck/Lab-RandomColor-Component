165c809bb199dc8f75384af640abcbef5184d540
TANNER MECK <steve.meck246@gmail.com>
2021-11-01 16:04:42 -0700

    presentation component added

--- src/containers/RandomColor.jsx
+++ src/containers/RandomColor.jsx
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
+import Box from '../presentation/Box';
 
 class RandomColor extends Component {
     state = { colors: ['blue', 'green', 'red', 'orange', 'yellow', 'purple', 'grey'],
-                currentColor: 'blue' }
+                currentColor: 'blue',
+                previousColor: '' }
 
     handleColor = () => { 
-        const randomNumber = Math.floor(Math.random() * 6)
+        const randomNumber = Math.floor(Math.random() * 7)
         setTimeout(()=> {
             this.setState({ currentColor: this.state.colors[randomNumber]})
+            // this.setState({ previousColor: this.state.currentColor})
         }, 1000)
         }
-    // componentDidMount(){
-    //     this.handleColor();
-    // }
 
     render() {
-        console.log(this.state.currentColor)
         return (
             <>
-            {this.handleColor()}
-                <h1>Hello From Random Color Generator</h1>
-                <div style={{ backgroundColor: this.state.currentColor, height: 300, width: 300 }}
-                    >
-                </div>
+                {this.handleColor()}
+
+                <h1>Random Color Generator</h1>
+
+                {/* {this.state.currentColor === this.state.previousColor && */}
+                {/* this code block */}
+                {/* <div style={{ backgroundColor: this.state.currentColor, height: 300, width: 300 }}
+                    
+                >
+                </div> */}
+                {/* to this code block */}
+                {/* } */}
+                <Box color={this.state.currentColor} />
+
             </>
         )
     }
---
+++ src/presentation/Box.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Box = ({ color }) => {
+    return (
+        <>
+        <div style={{ backgroundColor: color, height: 300, width: 300 }}
+                    
+        >
+        </div>
+        </>
+    )
+}
+export default Box;
