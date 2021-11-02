157ea3493afecda3ab610d01aa9269a887649e16
TANNER MECK <steve.meck246@gmail.com>
2021-11-01 18:05:46 -0700

    ci now passing

--- src/presentation/Box.jsx
+++ src/presentation/Box.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 const Box = ({ color }) => {
     return (
         <>
-        <div style={{ backgroundColor: color, height: 300, width: 300 }}
-                    
+        <div style={{ backgroundColor: color, height: 300, width: 300 }}        
         >
         </div>
         </>
