0494efbc401743df79dc9f54fc6c0a821e4edaa6
TANNER MECK <steve.meck246@gmail.com>
2021-11-01 17:55:05 -0700

    color rerendering with box presentation component

--- src/containers/RandomColor.jsx
+++ src/containers/RandomColor.jsx
@@ -4,33 +4,21 @@ import Box from '../presentation/Box';
 class RandomColor extends Component {
     state = { colors: ['blue', 'green', 'red', 'orange', 'yellow', 'purple', 'grey'],
                 currentColor: 'blue',
-                previousColor: '' }
+                previous: '' }
 
-    handleColor = () => { 
+    handleColor = () => {       
         const randomNumber = Math.floor(Math.random() * 7)
         setTimeout(()=> {
             this.setState({ currentColor: this.state.colors[randomNumber]})
-            // this.setState({ previousColor: this.state.currentColor})
         }, 1000)
-        }
+    }
 
     render() {
         return (
             <>
                 {this.handleColor()}
-
                 <h1>Random Color Generator</h1>
-
-                {/* {this.state.currentColor === this.state.previousColor && */}
-                {/* this code block */}
-                {/* <div style={{ backgroundColor: this.state.currentColor, height: 300, width: 300 }}
-                    
-                >
-                </div> */}
-                {/* to this code block */}
-                {/* } */}
                 <Box color={this.state.currentColor} />
-
             </>
         )
     }
